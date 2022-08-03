219aa4980dbbc40c5061622c7ee466f8b588e495
DangerX7 <63203272+DangerX7@users.noreply.github.com>
2022-08-03 17:33:46 +0300

    1st submit

---
+++ Simple Hourglass Draw App/ConsoleApp2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void DrawHourglass(int n)
+        {
+            int halfOfElement = n / 2;
+            int switchVariable = 1;
+
+            for (int i = 1; i <= n; i += switchVariable)
+            {
+                if (i > halfOfElement)
+                {
+                    switchVariable = -1;
+                    if (n % 2 == 0)
+                    {
+                        continue;
+                    }
+                }
+                if (i == 0)
+                {
+                    break;
+                }
+                for (int j = 0; j < i - 1; j++)
+                {
+                    Console.Write("  ");
+                }
+                for (int k = 0; k <= (n - i * 2) + 1; k++)
+                {
+                    Console.Write("**");
+                }
+                Console.WriteLine();
+
+
+
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Insert Hourglass Size ");
+            var hourglasscount = Convert.ToInt32(Console.ReadLine());
+            DrawHourglass(hourglasscount);
+        }
+    }
+}
